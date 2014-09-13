0f63b6b84b7be8f34860fb1ac3aa639d478b92b5
Ilshad Khabibullin <astoon.net@gmail.com>
2014-09-13 09:14:23 +0400

    clean up

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject miex "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2280"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.7.0"]]
+                 [org.clojure/clojurescript "0.0-2322"]
+                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
+                 [om "0.7.3"]]
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
   :source-paths ["src"]
   :cljsbuild
@@ -13,7 +13,7 @@
                 :output-dir "out"
                 :optimizations :none
                 :source-map true}}
-    :prod
+    :min
     {:source-paths ["src"]
      :compiler {:externs ["react/externs/react.js"]
                 :preamble ["react/react.min.js"]
